cbe09f7730fdc3290e67394eaf5ec47a22686afe
Daniel <danielochoa@outlook.com>
2019-08-21 17:42:16 -0500

    Análisis preliminar - 2

--- limpieza_datos/analisis_preliminar.py
+++ limpieza_datos/analisis_preliminar.py
@@ -25,4 +25,60 @@ basic_stats = data.describe()
 """ print(basic_stats) """
 # Tipos de datos
 data_types = data.dtypes
-print(data_types)
+""" print(data_types) """
+
+# // --------- MISSING VALUES --------- //
+data_null = pd.isnull(data["body"])
+""" print(data_null) """
+data_not_null = pd.notnull(data["body"])
+""" print(data_not_null) """
+
+# ravel => único arreglo de datos
+sum_data_null = pd.isnull(data["body"]).values.ravel().sum()  # suma los true
+""" print("Datos nulos => %d" % sum_data_null) """
+
+sum_data_not_null = pd.notnull(
+    data["body"]).values.ravel().sum()  # suma los true
+""" print("Datos no nulos => %d" % sum_data_not_null) """
+
+# // --------- BORRADO DE VALORES QUE FALTAN --------- //
+# axix = 0 => borra las fila
+# axix = 1 => borra las columna
+""" how = all => qué fila borrar (all => si todas las filas tienen NaN ||
+    any => Si al menos tiene una fila NaN) """
+
+# // --------- COMPUTAR LOS VALORES QUE FALTAN --------- //
+data2 = data  # Copia de la data
+# data_without_nan = data2.fillna(0) # Filas NaN = 0
+""" print(data_without_nan.head(10)) """
+
+# Reemplazar sólo la fila "body | home.dest ..."
+values = {'body': 0, 'home.dest': 'Desconocido', 'age': data2["age"].mean()}
+data_without_nan = data2.fillna(value=values)
+""" print(data_without_nan["age"]) """
+
+# // --------- VARIABLES DUMMY (de ayuda) --------- //
+# print(data["sex"]) # Categoría 0 => male | 1 => female
+dummy_sex = pd.get_dummies(data["sex"], prefix="sex")
+""" print(dummy_sex.head()) """
+# Columnas
+""" columns_name = data.columns.values.tolist()
+print(columns_name) """
+# Agregando el dummy a la data
+data_without_sex = data.drop(["sex"], axis=1)  # Eliminar la columna "sex"
+new_date = pd.concat([data_without_sex, dummy_sex], axis=1)  # agregar a data
+""" print(new_date) """
+
+# Función
+
+
+def create_dummies(data_frame, var_name):
+    dummy = pd.get_dummies(data_frame[var_name], prefix=var_name)
+    data_frame_without_var = data_frame.drop(var_name, axis=1)
+    new_data_frame = pd.concat([data_frame_without_var, dummy], axis=1)
+    return new_data_frame
+
+# Ejemplo => crear dummy al data2
+new_data = create_dummies(data2, "sex")
+columns_new_data = new_data.columns.values.tolist()
+print(columns_new_data)
